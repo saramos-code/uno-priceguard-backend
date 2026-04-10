6d823b910010345c0a20ffdfa5b2db8c9cd77b5f
saramos-code <sa.ramos.l89@gmail.com>
2026-04-09 18:17:35 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,5 @@
-const axios = require('axios');
-const { XMLParser } = require('fast-xml-parser');
-
-app.get('/precios', async (req, res) => {
-  try {
-    // La URL que sacamos de tu archivo .atomsvc
-    const urlDGEHM = "http://estadisticas.dgehm.gob.sv/WebHidroPublico/Reporte_precios_bajos"; 
-    
-    const response = await axios.get(urlDGEHM, { params: { 'rs:Format': 'ATOM' } });
-    
-    const parser = new XMLParser({ ignoreAttributes: false });
-    const jsonObj = parser.parse(response.data);
-
-    // Limpiamos los datos para que tu tabla los entienda
-    const estaciones = jsonObj.feed.entry.map(item => ({
-      nombre: item.content['m:properties']['d:Nombre_Estacion'],
-      ubicacion: item.content['m:properties']['d:Direccion'],
-      regular: item.content['m:properties']['d:Regular'],
-      especial: item.content['m:properties']['d:Especial'],
-      diesel: item.content['m:properties']['d:Diesel'],
-      fecha: new Date().toLocaleDateString()
-    }));
-
-    res.json({ precios: estaciones });
-  } catch (error) {
-    res.status(500).json({ error: "Error conectando con DGEHM" });
-  }
-});
+"dependencies": {
+  "axios": "^1.6.0",
+  "express": "^4.18.0",
+  "fast-xml-parser": "^4.3.0"
+}
